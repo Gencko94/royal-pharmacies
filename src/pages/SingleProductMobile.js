819db44c3797d2a51131d32b437bc95970a7524b
AhmadJS94 <gfox.piano@hotmail.com>
2020-10-11 11:52:27 +0300

    added floating action button on single product mobile

--- src/pages/SingleProductMobile.js
+++ src/pages/SingleProductMobile.js
@@ -13,6 +13,8 @@ import {
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { DataProvider } from '../contexts/DataContext';
 import { TiShoppingCart } from 'react-icons/ti';
+import { useInView } from 'react-intersection-observer';
+import { CSSTransition } from 'react-transition-group';
 export default function SingleProductMobile({
   match: {
     params: { id },
@@ -28,6 +30,8 @@ export default function SingleProductMobile({
   const [currentSlide, setCurrentSlide] = React.useState(0);
   const [quantity, setQuantity] = React.useState(1);
   const [detailsTab, setDetailsTab] = React.useState(0);
+
+  const [triggerRef, inView] = useInView();
 
   const isItemInCart = () => {
     const itemInCart = cartItems.find(item => data[0].id === item.id);
@@ -157,23 +161,30 @@ export default function SingleProductMobile({
               original Darksiders, exploring the origin of the Seven seals{' '}
             </li>
           </ul>
-          <div className="flex items-center mb-2">
+          <button className="bg-green-400 p-1 rounded mb-2  text-white flex items-center justify-center font-semibold ">
+            <span>
+              <AiOutlineHeart className="w-25p h-25p mr-2" />
+            </span>
+            Add to Wishlist
+          </button>
+          <div className="flex items-center ">
             <div className="mr-2 flex items-center">
               <h1 className=" mr-2 font-semibold">Quantity : </h1>
               <select
                 value={quantity}
                 onChange={e => setQuantity(e.target.value)}
-                className="select-mobile border-gray-400 border p-1 rounded"
+                className="select-mobile border-gray-400 border py-1 px-2 rounded"
               >
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
               </select>
             </div>
+
             {isItemInCart() ? (
               <button
                 onClick={() => removeItemFromCart(data[0])}
-                className="bg-red-700 text-gray-100  flex-1  p-1 rounded text-lg  flex items-center justify-center font-semibold "
+                className="bg-red-700 text-gray-100  flex-1  p-1 rounded   flex items-center justify-center font-semibold "
               >
                 <span>
                   <TiShoppingCart className="w-25p h-25p mr-2" />
@@ -183,7 +194,7 @@ export default function SingleProductMobile({
             ) : (
               <button
                 onClick={() => addItemToCart({ data: data[0], quantity })}
-                className="bg-blue-700 text-gray-100 flex-1  p-1 rounded text-lg  flex items-center justify-center font-semibold"
+                className="bg-blue-700 text-gray-100 flex-1  p-1 rounded   flex items-center justify-center font-semibold"
               >
                 <span>
                   <TiShoppingCart className="w-25p h-25p mr-2" />
@@ -192,37 +203,75 @@ export default function SingleProductMobile({
               </button>
             )}
           </div>
-          <button className="bg-green-400 p-1 rounded text-lg text-white flex items-center justify-center font-semibold ">
-            <span>
-              <AiOutlineHeart className="w-25p h-25p mr-2" />
-            </span>
-            Add to Wishlist
-          </button>
         </div>
       </div>
-      <div id="details" className="py-2">
-        <h1 className="text-xl font-semibold mb-1 px-3">Product Details</h1>
-        <div className="flex justify-center mb-2">
-          <button
-            onClick={() => setDetailsTab(0)}
-            className={`text-lg py-2 flex-1 text-center   ${
-              detailsTab === 0 && 'border-red-700 border-b-2 text-red-700'
-            }   bg-gray-400`}
-          >
-            Description
-          </button>
+      <div ref={triggerRef}>
+        <div id="details" className="py-2">
+          <h1 className="text-xl font-semibold mb-1 px-3">Product Details</h1>
+          <div className="flex justify-center mb-2">
+            <button
+              onClick={() => setDetailsTab(0)}
+              className={`text-lg py-2 flex-1 text-center   ${
+                detailsTab === 0 && 'border-red-700 border-b-2 text-red-700'
+              }   bg-gray-400`}
+            >
+              Description
+            </button>
 
-          <button
-            onClick={() => setDetailsTab(1)}
-            className={`text-lg py-2 flex-1 text-center   ${
-              detailsTab === 1 && 'border-red-700 border-b-2 text-red-700'
-            }   bg-gray-400`}
-          >
-            Specifications
-          </button>
+            <button
+              onClick={() => setDetailsTab(1)}
+              className={`text-lg py-2 flex-1 text-center   ${
+                detailsTab === 1 && 'border-red-700 border-b-2 text-red-700'
+              }   bg-gray-400`}
+            >
+              Specifications
+            </button>
+          </div>
+          <div className="px-3 text-sm">{data[0].description}</div>
         </div>
-        <div className="px-3 text-sm">{data[0].description}</div>
       </div>
+      <CSSTransition
+        in={inView}
+        timeout={200}
+        unmountOnExit={true}
+        classNames="floating-cart-button"
+      >
+        <div className=" bg-nav-secondary  floating-button">
+          <div className="mr-2 flex items-center">
+            <h1 className=" mr-2 font-semibold">Quantity : </h1>
+            <select
+              value={quantity}
+              onChange={e => setQuantity(e.target.value)}
+              className="select-mobile border-gray-400 border py-1 px-2 rounded"
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+          </div>
+          {isItemInCart() ? (
+            <button
+              onClick={() => removeItemFromCart(data[0])}
+              className="bg-red-700 text-gray-100  flex-1  p-1 rounded   flex items-center justify-center font-semibold "
+            >
+              <span>
+                <TiShoppingCart className="w-25p h-25p mr-2" />
+              </span>
+              Remove From Cart
+            </button>
+          ) : (
+            <button
+              onClick={() => addItemToCart({ data: data[0], quantity })}
+              className="bg-blue-700 text-gray-100 flex-1  p-1 rounded  flex items-center justify-center font-semibold"
+            >
+              <span>
+                <TiShoppingCart className="w-25p h-25p mr-2" />
+              </span>
+              Add to Cart
+            </button>
+          )}
+        </div>
+      </CSSTransition>
     </div>
   );
 }
