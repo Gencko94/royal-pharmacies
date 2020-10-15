e5070a190774875e3fba19d92235a04f2f0132a0
AhmadJS94 <gfox.piano@hotmail.com>
2020-10-15 15:40:57 +0300

    fixed some coloring issues

--- src/components/NavbarComponents/SearchBar.js
+++ src/components/NavbarComponents/SearchBar.js
@@ -7,17 +7,17 @@ export default function SearchBar() {
   const { isLightTheme } = React.useContext(DataProvider);
   return (
     <div
-      className={`flex  mr-3 py-2 rounded  relative  ${
+      className={`flex  mr-3 py-0 rounded overflow-hidden  relative  ${
         isLightTheme
           ? 'bg-nav-cat-light text-nav-cat-text-light'
           : 'bg-nav-cat-dark text-nav-cat-text-dark'
       }    `}
     >
-      <div className="grid place-items-center px-1 ">
+      <div className="grid place-items-center p-2 bg-nav-cat-light border-r mr-1 ">
         <BiSearch className=" w-5 h-5" />
       </div>
       <input
-        className={`flex-grow   ${
+        className={`flex-grow  p-2 ${
           isLightTheme
             ? 'bg-nav-cat-light text-nav-cat-text-light placeholder-gray-700'
             : 'bg-nav-cat-dark text-nav-cat-text-dark placeholder-gray-500'
